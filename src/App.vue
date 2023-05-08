681517459a98838b708d140ba5cfd5cb6bea7a94
asgabrielsilva <gabrielalmeida290905@gmail.com>
2023-05-08 15:59:10 -0300

    Começo do projeto 2

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+ import { ref } from 'vue'
+
+ const produtos = [
+    {
+        id: 1,
+        nome: 'Camiseta',
+        preco: 49.90
+    },
+    {
+        id: 2,
+        nome: 'Calça',
+        preco: 99.90
+    },
+    {
+        id: 3,
+        nome: 'Meia',
+        preco: 9.90
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        preco: 199.90
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        preco: 99.90
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        preco: 299.90
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        preco: 79.90
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        preco: 19.90
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90
+    }
+]
+
+</script>
+
+<template>
+<div v-for="(item, index) in produtos" :key="item.id">
+  <p>Nome: {{ item.name.value }}</p>
+  <p>Preço: {{ item.preco }}</p>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
